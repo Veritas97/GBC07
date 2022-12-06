1303a63441b7832d3c98ff3728d5f74e0a3c4b8b
Veritas97 <lykovcl@gmail.com>
2022-12-06 12:47:32 +0900

    Answer Task48

---
+++ Task48/Program.cs
@@ -0,0 +1,35 @@
+﻿int[,] CreateMatrixRandomInt(int SizeX, int SizeY, int min, int max)
+{
+    int[,] matrix = new int[SizeX, SizeY];
+    // Random random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4} ");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+
+        Console.WriteLine("|");
+    }
+}
+
+int[,] array2D = CreateMatrixRandomInt(3, 4, -50, 50);
+PrintMatrix(array2D);
