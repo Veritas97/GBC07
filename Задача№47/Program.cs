714de057c8739ad28e0e32a722c37275b5eedfbe
Veritas97 <lykovcl@gmail.com>
2022-12-06 13:21:25 +0900

    Answer Task47

---
+++ Задача№47/Program.cs
@@ -0,0 +1,34 @@
+﻿double[,] CreateMatrixRandomInt(int SizeX, int SizeY, int min, int max)
+{
+    double[,] matrix = new double[SizeX, SizeY];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+
+        Console.WriteLine("|");
+    }
+}
+
+double[,] array2D = CreateMatrixRandomInt(3, 4, -10, 10);
+PrintMatrix(array2D);
